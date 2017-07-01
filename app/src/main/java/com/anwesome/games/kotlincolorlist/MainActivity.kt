01fa6e86e8c5f04e95104292ddc9eca91ba49b99
Anwesh43 <anweshthecool0@gmail.com>
2017-07-01 16:08:29 +0530

    adding onclicklistener in activity

--- app/src/main/java/com/anwesome/games/kotlincolorlist/MainActivity.kt
+++ app/src/main/java/com/anwesome/games/kotlincolorlist/MainActivity.kt
@@ -3,8 +3,9 @@ package com.anwesome.games.kotlincolorlist
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnClickListner {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         customView.addColor(Color.parseColor("#0277BD"))
         customView.addColor(Color.parseColor("#0D47A1"))
         customView.addColor(Color.parseColor("#e53935"))
+        customView.clickListener = this
         setContentView(customView)
+    }
+    override fun onClick(index:Int?) {
+        Toast.makeText(this,"clicked $index",Toast.LENGTH_SHORT).show()
     }
 }
